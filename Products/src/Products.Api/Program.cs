85625b4aa314d9d790df6a1dc7aa623df3b3332b
Samuel Rangel <samuelrangel27@gmail.com>
2026-08-05 18:43:52 -0600

    Add Dockerfile for Products API

--- Products/src/Products.Api/Program.cs
+++ Products/src/Products.Api/Program.cs
@@ -6,16 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
